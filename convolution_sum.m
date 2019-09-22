6fc2bea6cc6560cfe3353be04a67c7716234ab6a
Nadim-Mahmud <mahmudnadim16@gmail.com>
2019-09-22 23:04:48 +0600

    convolution completed

--- convolution_sum.m
+++ convolution_sum.m
@@ -7,26 +7,34 @@ clear
 % prompt2 = 'Enter the x value ?';
 % h = input(prompt2);
 
-x = [3 2 1 2];
-h = [1 2 1 2];
+x = [2 1 -1 -2 3];
+h = [1,2,0,3];
+
+zxp = 3
+zyp = 2;
 
 lnx = length(x);
 lnh = length(h);
 ln = length(x) + length(h)-1;
 
-in=0;
-for i = 1 : ln
-    tmp = 0; 
-    for j = 1:lnh
-        p = -1*j + i;
-        if (p>=0&&p<lnx)
-            tmp =  tmp + h(j)*x(p+1);
-        end
-    end
-    in = in + 1;
-    sum(in) = tmp; 
-end
-    
-disp(sum);
+x = [x,zeros(1,lnx)];
+h = [h,zeros(1,lnh)];
 
+for i = 1 : ln
+  y(i) = 0;
+  for j = 1 : lnx
+    if (i-j+1>0)
+      y(i) = y(i) + x(j)*h(i-j+1);  
+    end 
+  end
+end
+
+disp(y);
+%Z = conv(x,h)
+
+xfy = -(zxp+zyp-2):1:ln -(zxp+zyp-2)-1;
+
+subplot(2,2,1);
+title("X = ");
+stem(xfy,y);
 
